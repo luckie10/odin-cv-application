5540089536a6f224709ee8c6ee1608918a91f5a5
Boban Kilvin <btrajcevski10@gmail.com>
2023-11-28 03:11:09 -0500

    Add ability to edit heading contents

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+import InputField from "../InputField";
+import "./Heading.style.css";
+
 function Heading() {
+  const [name, setName] = useState("Full Name");
+  const [phoneNumber, setPhoneNumber] = useState("###-###-####");
+  const [email, setEmail] = useState("email@foo.bar");
+
   return (
     <>
-      <h1>Name</h1>
-      <div>
-        <p>Phone Number</p>
-        <p>Email</p>
-      </div>
+      <header>
+        <h1>
+          <InputField type="input" state={name} setState={setName} />
+        </h1>
+        <div className="contact-info">
+          <InputField state={phoneNumber} setState={setPhoneNumber} />
+          •
+          <InputField state={email} setState={setEmail} />{" "}
+        </div>
+      </header>
     </>
   );
 }
---
+++ src/components/InputField/InputField.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./InputField.style.css";
+
+export default function InputField({ state, setState, type = "input" }) {
+  const [isEditable, setIsEditable] = useState(false);
+  const InputType = type;
+
+  return (
+    <>
+      {isEditable ? (
+        <InputType
+          class="inherit"
+          value={state}
+          onChange={(event) => setState(event.target.value)}
+          onBlur={() => setIsEditable(false)}
+          autoFocus
+        />
+      ) : (
+        <span onClick={() => setIsEditable(true)}>{state}</span>
+      )}
+    </>
+  );
+}
