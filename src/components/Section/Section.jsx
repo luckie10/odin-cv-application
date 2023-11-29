4dac790d514b63239ce8614370ec60c14f437756
Boban Kilvin <btrajcevski10@gmail.com>
2023-11-29 01:15:15 -0500

    Add some style to Section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,9 +1,11 @@
+import "./Section.style.css";
+
 function Section({ name, children }) {
   return (
-    <>
+    <div className={"section " + name.toLowerCase()}>
       <h2>{name}</h2>
       <div>{children}</div>
-    </>
+    </div>
   );
 }
 
