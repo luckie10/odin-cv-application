1bc6ef046d0b97a8eaf0733c5671140ec40c8e26
Boban Kilvin <btrajcevski10@gmail.com>
2023-11-29 02:47:00 -0500

    Move exampleExperinces to Experinces component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,23 +6,6 @@ import Experience from "../Experience/Experience";
 import References from "../References/References";
 
 function App() {
-  const testExperiences = [
-    {
-      id: 0,
-      company: "HFHS",
-      position: "RN",
-      date: "2017-Present",
-      duties: "blah",
-    },
-    {
-      id: 1,
-      company: "HBPO",
-      position: "GL",
-      date: "2009-2014",
-      duties: "blah blah",
-    },
-  ];
-
   const testReferences = [
     { id: 0, name: "Me", number: "519-###-####", email: "blah@blahbl.ah" },
     { id: 1, name: "Myself", number: "313-###-####", email: "foo@b.ar" },
@@ -35,7 +18,7 @@ function App() {
         <Education />
       </Section>
       <Section name="Experience">
-        <Experience experiences={testExperiences} />
+        <Experience />
       </Section>
       <Section name="References">
         <References references={testReferences} />
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,4 +1,31 @@
-function Experience({ experiences }) {
+import { useState } from "react";
+
+const exampleExperinces = [
+  {
+    id: 0,
+    company: "Department of Civil Engineering, University of Illinois",
+    position: "Graduate Research Assistant",
+    date: "20XX-Present",
+    duties: [
+      "Design and execute small-scale testing to validate control algorithms derived to simulate seismic force-resistance.",
+      "Contribute to multi-disciplinary project aimed at developing visualizations and simulations to predict seismic force damage to various materials.",
+      "Collaborate and coordinate with faculty, staff scientists, and fellow graduate students across departments.",
+    ],
+  },
+  {
+    id: 1,
+    company: "Departamento de Ingeniería, UNSJ",
+    position: "Undergraduate Consultant",
+    date: "20XX",
+    duties: [
+      "Selected by the General Director of the City Planning Department of San Juan, to participate in the structural analysis and seismic assessment of the Dr. Guillermo Rawson Hospital, one of the largest construction projects to date in the most hazardous seismic area in Argentina",
+      "Selected by the General Director of the City Planning Department of San Juan, to participate in the structural analysis and seismic assessment of the Dr. Guillermo Rawson Hospital, one of the largest construction projects to date in the most hazardous seismic area in Argentina",
+    ],
+  },
+];
+
+function Experience() {
+  const [experiences, setExperiences] = useState(exampleExperinces);
   return (
     <>
       {experiences.map(({ id, company, position, date, duties }) => (
