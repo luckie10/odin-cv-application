664e7611e79536e42e0070e6cddd8d4a65111866
Boban Kilvin <btrajcevski10@gmail.com>
2023-12-13 02:27:17 -0500

    Add input fields to each Reference element

--- src/components/References/References.jsx
+++ src/components/References/References.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { generateInputField } from "../helpers";
 
 const exampleReferences = [
   {
@@ -24,16 +25,39 @@ const exampleReferences = [
 function References() {
   const [references, setReferences] = useState(exampleReferences);
 
+  function genRefElement(reference, generatorParams) {
+    const elements = {};
+
+    Object.keys(reference).forEach((key) => {
+      elements[key] = generateInputField(key, ...generatorParams);
+    });
+
+    return elements;
+  }
+
+  function generateReference(reference, index) {
+    const generatorParams = [index, reference.id, references, setReferences];
+    const { name, title, department, organization, number, email } =
+      genRefElement(reference, generatorParams);
+
+    return (
+      <div key={reference.id}>
+        <h3>
+          {name}, {title}
+        </h3>
+        <p>{department}</p>
+        <p>{organization}</p>
+        <p>
+          {number}, {email}
+        </p>
+      </div>
+    );
+  }
+
   return (
     <>
-      {references.map(
-        ({ id, name, title, department, organization, number, email }) => (
-          <div key={id}>
-            <h3>{name}</h3>
-            <p>{number}</p>
-            <p>{email}</p>
-          </div>
-        ),
+      {references.map((reference, index) =>
+        generateReference(reference, index),
       )}
     </>
   );
